52d9acb24f262fce2be179fd2002015db0c23c9b
abinop <abinop@gmail.com>
2024-01-08 11:21:18 +0200

    Refactor handleCharacteristicValueChanged_sensor function and update deviceCircleAssociation

--- src/App copy.js
+++ src/App copy.js
@@ -5,33 +5,75 @@ import './style.css';
 
 function App() {
   const [characteristic, setCharacteristic] = useState(null);
+  const [isListening, setIsListening] = useState(false);
+  const [interpretedValue, setInterpretedValue] = useState(null); // New state for the interpreted value
+  const marioRef = useRef(null); // Ref for the Mario character
   const serviceUUID = '00001523-1212-efde-1523-785feabcd123';
   const kSensorCharacteristicUUID = '00001524-1212-efde-1523-785feabcd123';
   const kCommandCharacteristicUUID = '00001528-1212-efde-1523-785feabcd123';
   const [server, setServer] = useState(null); // State for the server
   const [selectedColor, setSelectedColor] = useState('');
   const [luminosity, setLuminosity] = useState(0);
+  const [bleError, setBleError] = useState(null);
   const [connectedDevices, setConnectedDevices] = useState([]);
   const [deviceCircleAssociation, setDeviceCircleAssociation] = useState({});
 
 
+  const style = {
+    display: 'none', // initially hidden
+
+  };
+
+  // const handleConnect = async () => {
+  //   try {
+  //     console.log('Scanning for devices...');
+  //     const device = await scanForDevices();
+  //     const connectedServer = await device.gatt.connect();
+  //     setServer(connectedServer); // Set the server state
+
+  //     const char_sensor = await startNotifications(connectedServer, serviceUUID,
+  //       kSensorCharacteristicUUID, handleCharacteristicValueChanged_sensor);
+  //     setCharacteristic(char_sensor);
+
+  //     setIsListening(true); // Update the isListening
+  //   } catch (error) {
+  //     console.error('Connection failed:', error.message);
+  //     setBleError(error); // Set the error state
+
+
+
+
+  //   }
+  // };
+
+  // const handleStartListening = () => {
+  //   if (characteristic) {
+  //     console.log('Starting notifications...');
+  //     setIsListening(true);
+  //   }
+  // };
+
+  // const handleStopListening = () => {
+  //   if (characteristic) {
+  //     console.log('Stopping notifications...');
+  //     stopNotifications(characteristic);
+  //     setIsListening(false);
+  //   }
+  // };
 
   // Callback function for handling characteristic value changes
   function handleCharacteristicValueChanged_sensor(event, deviceId) {
     const value = event.target.value;
     const intValue = value.getUint8(0);
-    
-    setDeviceCircleAssociation(prevAssociations => {
-      const updatedAssociations = {
-        ...prevAssociations,
-        [deviceId]: {
-          ...prevAssociations[deviceId],
-          forceValue: intValue
-        }
-      };
-  
-      return updatedAssociations;
-    });
+
+    // Update the force value for the specific device in deviceCircleAssociation
+    setDeviceCircleAssociation(prevAssociations => ({
+      ...prevAssociations,
+      [deviceId]: {
+        ...prevAssociations[deviceId],
+        forceValue: intValue
+      }
+    }));
   }
 
   // Function to write color to the characteristic
@@ -108,8 +150,7 @@ function App() {
       setServer(connectedServer); // Set the server state
 
       const char_sensor = await startNotifications(connectedServer, serviceUUID,
-          kSensorCharacteristicUUID, (event) => handleCharacteristicValueChanged_sensor(event, device.id));
-        
+        kSensorCharacteristicUUID, handleCharacteristicValueChanged_sensor);
       setCharacteristic(char_sensor);
 
       // Add the new device to the connected devices list and create an association with the circle
@@ -160,8 +201,71 @@ function App() {
 
 
 
+  //   return (
+  //     <div className="p-4">
+  //       <h1 className="text-xl font-bold mb-4">Interacting with Cosmo</h1>
+  //       <div className="mb-4">
+  //         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleConnect}>Connect to Cosmo</button>
+  //         <button onClick={handleStartListening} disabled={!characteristic || isListening}>Start Listening</button>
+  //         <button onClick={handleStopListening} disabled={!characteristic || !isListening}>Stop Listening</button>
+
+  //       </div>
+
+  //       {interpretedValue !== null && (
+  //         <div className="space-y-40">
+  //           <div>
+  //             <p className="text-lg">Press Value: {interpretedValue}</p>
+  //           </div>
+  //           <div>
+  //             <label htmlFor="color-select" className="block mb-2">Set Color: </label>
+  //             <select id="color-select" value={selectedColor} onChange={handleColorChange} className="border border-gray-300 rounded px-4 py-2 focus:border-blue-500">
+  //               <option value="--none--">--none--</option>
+  //               <option value="blue">Blue</option>
+  //               <option value="red">Red</option>
+  //               <option value="green">Green</option>
+  //               <option value="white">White</option>
+  //               <option value="orange">Orange</option>
+  //               <option value="darkYellow">Dark Yellow</option>
+  //               <option value="purple">Purple</option>
+  //               <option value="pink">Pink</option>
+  //               <option value="fuchsia">Fuchsia</option>
+  //               <option value="turquoise">Turquoise</option>
+  //               <option value="lightGreen">Light Green</option>
+  //               <option value="fluorescentGreen">Fluorescent Green</option>
+  //               <option value="yellow">Yellow</option>
+  //               <option value="lightPurple">Light Purple</option>
+  //               <option value="iceWhite">Ice White</option>
+  //             </select>
+  //           </div>
+  //           <div>
+  //             <label htmlFor="luminosity-range" className="block mb-2">Set Luminosity (0-64): </label>
+  //             <input
+  //               type="range"
+  //               id="luminosity-range"
+  //               min="0"
+  //               max="64"
+  //               value={luminosity}
+  //               onChange={handleLuminosityChange}
+  //               className="w-full"
+  //             />
+  //             <span className="text-lg">{luminosity}</span>
+  //             <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded ml-4" onClick={handleSetLuminosity}>Set Luminosity</button>
+  //           </div>
+  //         </div>
+  //       )}
+
+  //       {bleError && <BrowserSpecificErrorMessage error={bleError} />}
+
+
+
+  //     </div>
+  //   );
+
+  // }
+
   return (
     <div className="p-4 space-y-4">
+      <h1 className="text-xl font-bold mb-4">Interact with Devices</h1>
       <div className="flex justify-around">
         {colors.map((color, index) => {
           // Determine if the circle should be active based on the number of connected devices
