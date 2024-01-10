02a20867ab816f8b5939f86df350cb002be12236
abinop <abinop@gmail.com>
2024-01-10 11:27:53 +0200

    Refactor circle styling and add border and plus color classes

--- src/HomePage.js
+++ src/HomePage.js
@@ -6,27 +6,32 @@ function HomePage({ colors, connectedDevices, deviceCircleAssociation, handleCon
 
   return (
     <div className="p-4 space-y-4">
-      <div className="flex justify-around">
-        {colors.map((color, index) => {
-          const isActive = index < numberOfConnectedDevices;
-          const isNextToConnect = index === numberOfConnectedDevices;
-          const circleClasses = `circle ${isActive ? `circle-${color}` : 'circle-disconnected'} ${isNextToConnect ? "circle-next" : ""}`;
-          const device = connectedDevices[index];
-          const deviceId = device?.id;
-          const forceValue = deviceId ? deviceCircleAssociation[deviceId]?.forceValue : undefined;
+      <div className="flex justify-left gap-2">
+        <div className="flex justify-around">
+          {colors.map((color, index) => {
+            const isActive = index < numberOfConnectedDevices;
+            const isNextToConnect = index === numberOfConnectedDevices;
+            const nextColor = colors[index] || colors[0]; // Get next color or loop back to start
+            const circleClasses = `circle ${isActive ? `circle-${color}` : 'circle-disconnected'} ${isNextToConnect ? `border-${nextColor}` : ''}`;
+            const plusClasses = `circle-plus ${isNextToConnect ? `plus-${nextColor}` : ''}`;
+            const device = connectedDevices[index];
+            const deviceId = device?.id;
+            const forceValue = deviceId ? deviceCircleAssociation[deviceId]?.forceValue : undefined;
 
-          return (
-            <div key={index} className={circleClasses} onClick={() => isNextToConnect && handleConnectToDevice()}>
-              {isNextToConnect ? (
-                <span className="circle-plus">+</span>
-              ) : isActive && forceValue !== undefined ? (
-                <p>
-                <strong>{forceValue}</strong><br/>Force value
-                </p>
-              ) : null}
-            </div>
-          );
-        })}
+            return (
+              <div key={index} className={circleClasses} onClick={() => isNextToConnect && handleConnectToDevice()}>
+                {isNextToConnect ? (
+                  <span className={plusClasses}>+</span>
+                ) : isActive && forceValue !== undefined ? (
+                  <p>
+                    <strong>{forceValue}</strong><br />Force value
+                  </p>
+                ) : null}
+              </div>
+            );
+          })}
+        </div>
+
       </div>
       <div className="text-center">
         Cosmoids: {numberOfConnectedDevices}
